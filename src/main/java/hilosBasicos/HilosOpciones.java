af4a86dcf294e484c1a2734917ba97d3f47d54aa
sadohu <sadohu26@yahoo.com>
2023-11-18 07:21:30 -0500

    [Add] Pregunta 1.A.2

--- src/main/java/hilosBasicos/HilosOpciones.java
+++ src/main/java/hilosBasicos/HilosOpciones.java
@@ -13,23 +13,30 @@ public class HilosOpciones extends Thread{
 		case 1:
 			opcion1();
 			break;
+		case 2:
+			opcion2();
+			break;
 		default:
 			break;
 		}
 	}
 	
 	private void opcion1(){
-		int count = 0;
 		for (int i = 1; i <= 20; i++) {
 			if(i % 2 == 0){
 				System.out.println("Opción 1: " + i);
-				count += i;
 				try {
 					sleep(1000);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
 			}
+		}
+	}
+	
+	private void opcion2(){
+		for (char i = 'a'; i <= 'z' ; i++) {
+			System.out.println("Opción 2: " + i);
 		}
 	}
 }
--- src/main/java/hilosBasicos/HilosPrincipal.java
+++ src/main/java/hilosBasicos/HilosPrincipal.java
@@ -5,6 +5,8 @@ public class HilosPrincipal {
 	public static void main(String[] args) {
 		HilosOpciones hilos1 = new HilosOpciones(1);
 		hilos1.start();
+		HilosOpciones hilos2 = new HilosOpciones(2);
+		hilos2.start();
 
 	}
 
