cbcd96a614438de3e3fc91f997c566b764fc78b0
sadohu <sadohu26@yahoo.com>
2023-11-18 11:11:56 -0500

    [Add] 1.C Server-Client Implements

--- src/main/java/pc_servidor/Servidor.java
+++ src/main/java/pc_servidor/Servidor.java
@@ -1,5 +1,35 @@
 package pc_servidor;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Servidor {
 
+	public static void main(String[] args) throws ClassNotFoundException {
+		try {
+			ServerSocket server = new ServerSocket(4500);
+			System.out.println("Servivor a la espera de peticiones.");
+			Socket client = server.accept();
+			
+			ObjectInputStream requets = new ObjectInputStream(client.getInputStream());
+			System.out.println("Requets recibido.");
+			
+			String message = (String) requets.readObject();
+			System.out.println("Mensaje desde el cliente: " + message);
+			
+			ObjectOutputStream response = new ObjectOutputStream(client.getOutputStream());
+			response.writeObject("Hola cliente, el presidente fue Ramón Castilla.");
+			System.out.println("Respuesta enviada.");
+
+			client.close();
+			server.close();
+			System.out.println("Servivor apagado.");
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
